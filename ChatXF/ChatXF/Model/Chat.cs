39dc29ff904742d44cceb73b8d238a965c1712f3
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-08-05 17:33:23 -0300

    Criação das classes do modelo

---
+++ ChatXF/ChatXF/Model/Chat.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatXF.Model {
+    public class Chat {
+        public int id { get; set; }
+        public string nome { get; set; }
+    }
+}
