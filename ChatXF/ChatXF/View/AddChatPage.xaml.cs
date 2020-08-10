a39b0444e80ded1688f83b28bc4b8be3e32e547f
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-08-10 14:58:15 -0300

    Implementação da tela de cadastro de chats

--- ChatXF/ChatXF/View/AddChatPage.xaml.cs
+++ ChatXF/ChatXF/View/AddChatPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChatXF.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace ChatXF.View {
     public partial class AddChatPage : ContentPage {
         public AddChatPage() {
             InitializeComponent();
+            BindingContext = new AddChatViewModel();
         }
     }
 }
--- ChatXF/ChatXF/ViewModel/AddChatViewModel.cs
+++ ChatXF/ChatXF/ViewModel/AddChatViewModel.cs
@@ -1,8 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
+using Xamarin.Forms;
+using ChatXF.Model;
+using ChatXF.Service;
+using ChatXF.View;
 
 namespace ChatXF.ViewModel {
-    class AddChatViewModel {
+    public class AddChatViewModel : INotifyPropertyChanged {
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName) {
+            if(PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public string Nome { get; set; }
+
+        private string _Error;
+        public string Error {
+            get { return _Error; }
+            set {
+                _Error = value;
+                OnPropertyChanged("Error");
+            }
+        }
+
+        public Command SalvarCommand { get; set; }
+
+        public AddChatViewModel() {
+            SalvarCommand = new Command(AttemptSalvar);
+        }
+
+        private void AttemptSalvar() {
+            if(Nome == null || Nome.Length == 0) {
+                Error = "Preencha o campo nome.";
+                return;
+            }
+            Salvar();
+        }
+
+        private void Salvar() {
+            var chat = new Chat() {
+                nome = Nome
+            };
+            bool ok = new ChatService().InsertChat(chat);
+            if (ok) {
+                GoBackAndUpdate();
+            } else {
+                Error = "Erro ao cadastrar o chat. Tente novamente mais tarde";
+            }
+        }
+
+        private void GoBackAndUpdate() {
+            ((NavigationPage)App.Current.MainPage).PopAsync();
+            var nav = (NavigationPage)App.Current.MainPage;
+            var chatsViewModel = ((ChatListPage)nav.CurrentPage).BindingContext as ChatListViewModel;
+            if (chatsViewModel.AtualizarCommand.CanExecute(null))
+                chatsViewModel.AtualizarCommand.Execute(null);
+        }
+
     }
 }
